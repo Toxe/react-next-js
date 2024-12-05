49d8862133560f9a55a91c0bb8936d11ca8e4a50
Toxe <toxe@toxe.net>
2024-12-05 15:02:25 +0100

    Add page1: a simple page without formatting

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -5,6 +6,9 @@ export default function Home() {
         <div className={styles.page}>
             <main className={styles.main}>
                 <h1>Main Page</h1>
+                <ul>
+                    <li><Link href="/page1">Page 1: Simple, unformatted page</Link></li>
+                </ul>
             </main>
             <footer className={styles.footer}>
                 <a href="https://nextjs.org/docs/app/getting-started/installation">based on Next.js Getting Started</a>
