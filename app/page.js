2805c060e16bbf98f726963d1ebc188592c07dc0
Toxe <toxe@toxe.net>
2024-12-05 14:49:46 +0100

    Reformat

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Main Page</h1>
-      </main>
-      <footer className={styles.footer}>
-        <a href="https://nextjs.org/docs/app/getting-started/installation">based on Next.js Getting Started</a>
-      </footer>
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1>Main Page</h1>
+            </main>
+            <footer className={styles.footer}>
+                <a href="https://nextjs.org/docs/app/getting-started/installation">based on Next.js Getting Started</a>
+            </footer>
+        </div>
+    );
 }
